b9361e3ebd545d515791ce7cd6febc86839ee6f0
Mikhail <bskfrost3@yandex.ru>
2021-04-19 15:08:43 +0700

    Add all buttons

--- CalculatorUpDown/Sources/Scenes/LayoutIBViewController.swift
+++ CalculatorUpDown/Sources/Scenes/LayoutIBViewController.swift
@@ -9,6 +9,31 @@ import UIKit
 
 class LayoutIBViewController: UIViewController {
 
+    @IBOutlet var divisionButton: UIButton!
+    @IBOutlet var multiplyButton: UIButton!
+    @IBOutlet var subtractionButton: UIButton!
+    @IBOutlet var additionButton: UIButton!
+    @IBOutlet var equalityButton: UIButton!
+
+    @IBOutlet var percentButton: UIButton!
+    @IBOutlet var nineButton: UIButton!
+    @IBOutlet var sixButton: UIButton!
+    @IBOutlet var threeButton: UIButton!
+    @IBOutlet var commaButton: UIButton!
+
+    @IBOutlet var plusMinusButton: UIButton!
+    @IBOutlet var eightButton: UIButton!
+    @IBOutlet var fiveButton: UIButton!
+    @IBOutlet var twoButton: UIButton!
+
+    @IBOutlet var dischargeButton: UIButton!
+    @IBOutlet var sevenButton: UIButton!
+    @IBOutlet var fourButton: UIButton!
+    @IBOutlet var oneButton: UIButton!
+
+    @IBOutlet var zeroButton: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
