a5a9adaffb4e308b7fb6e5fab6c7043f46768f41
Mikhail <bskfrost3@yandex.ru>
2021-04-19 20:14:03 +0700

    Create ConstreitVC and Constreit.storyboard

---
+++ CalculatorUpDown/Sources/Scenes/ConstreitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ConstreitViewController.swift
+//  CalculatorUpDown
+//
+//  Created by mac on 19.04.2021.
+//
+
+import UIKit
+
+class ConstreitViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
